96ba31622549378743f638b56f4c6405ab78980a
Hanks <zhanghan.me@gmail.com>
2017-03-10 19:06:26 +0800

    get all issues of alibaba/weex

---
+++ fetch.js
@@ -0,0 +1,39 @@
+const GitHub = require('github-api/dist/GitHub.js')
+const jsonfile = require('jsonfile')
+
+const gh = new GitHub({
+  username: 'Hanks10100',
+  password: '**********'
+})
+
+function getAllIssues (agent) {
+  console.log(` => fetching the issue list ...`)
+  agent.listIssues({
+    state: 'all'
+  }, (err, issueList) => {
+    if (err) {
+      console.log(` => failed to fetch the issue list`)
+      return
+    }
+
+    const issues = Array.from(issueList).filter(item => !item.pull_request)
+    console.log(` => got ${issues.length} issues`)
+    issues.forEach(issue => {
+      console.log(` => fetch the comments of ${issue.number}`)
+      agent.listIssueComments(issue.number, (error, comments) => {
+        if (error) {
+          console.log(` => failed to fetch the comments of ${issue.number}`)
+        } else {
+          console.log(` => got ${comments.number} comments of  ${issue.number}`)
+          issue.comments = comments
+        }
+        jsonfile.spaces = 2
+        jsonfile.writeFile(`issues/${issue.number}.json`, issue, () => {
+          console.log(` => write issues/${issue.number}.json`)
+        })
+      })
+    })
+  })
+}
+
+getAllIssues(gh.getIssues('alibaba', 'weex'))
